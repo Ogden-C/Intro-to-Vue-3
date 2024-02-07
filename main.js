88f863da6347fddd193396813a724b6d9d8cee04
Ogden-C <106078496+Ogden-C@users.noreply.github.com>
2024-02-07 15:59:44 -0500

    Refactor product display in index.html

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
+<<<<<<< Updated upstream
     data() {
         return {
             product: 'Socks'
+=======
+    data(){
+        return {
+            product: 'Socks',
+            description: 'A pair of warm, fuzzy socks'
+>>>>>>> Stashed changes
         }
     }
 })
