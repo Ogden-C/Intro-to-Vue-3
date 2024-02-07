2696657a08f2022010896cdae033377397b80d57
Ogden-C <106078496+Ogden-C@users.noreply.github.com>
2024-02-07 16:03:22 -0500

    Revert "Refactor product display in index.html"
    This reverts commit 88f863da6347fddd193396813a724b6d9d8cee04.

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const app = Vue.createApp({
-<<<<<<< Updated upstream
     data() {
         return {
             product: 'Socks'
-=======
-    data(){
-        return {
-            product: 'Socks',
-            description: 'A pair of warm, fuzzy socks'
->>>>>>> Stashed changes
         }
     }
 })
